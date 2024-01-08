6daff7542d6e3606232eea6552bc37f2675bee0e
coy <coyoteandbird@gmail.com>
2024-01-07 21:39:55 -0500

    basic movement

---
+++ Sprite2D.gd
@@ -0,0 +1,4 @@
+extends Sprite2D
+
+func _process(delta):
+	look_at(get_global_mouse_position())
--- character.gd
+++ character.gd
@@ -1,28 +1,66 @@
 extends CharacterBody2D
+@onready var rightRaycast = $RightWalljumpRaycast
+@onready var leftRaycast = $LeftWalljumpRaycast
+
+@export var gravity = 980
+@export var acceleration = 500
+@export var max_speed = 1500
+@export var friction = 500
+@export var jump_initial_speed = -700
+@export var walljump_initial_vertical_speed = -500
+@export var walljump_initial_horizontal_speed = -200
+@export var wallslide_acceleration = 0
+
+var direction
+var has_double_jump = false
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func get_input() -> float:
+	return Input.get_axis("move_left", "move_right")
 
+func is_wall_sliding():
+	return (
+		(rightRaycast.is_colliding() or leftRaycast.is_colliding())
+		and not is_on_floor()
+		and velocity.y > 0
+	)
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+func handle_wall_jump():
+	if (
+		(rightRaycast.is_colliding() or leftRaycast.is_colliding()) and
+		Input.is_action_just_pressed("jump") and
+		not is_on_floor() 
+	):
+		velocity.y = walljump_initial_vertical_speed
+		if leftRaycast.is_colliding():
+			velocity.x = -1 * walljump_initial_horizontal_speed
+		else:
+			velocity.x = walljump_initial_horizontal_speed
+		has_double_jump = true
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func handle_jump():
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_initial_speed
+		has_double_jump = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func handle_double_jump():
+	if not is_on_floor() and Input.is_action_just_pressed("jump") and has_double_jump:
+		velocity.y = jump_initial_speed
+		has_double_jump = false
+
+func _process(delta):
+	direction = get_input()
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * max_speed, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+	
+	if is_wall_sliding() :
+		velocity.y = 200
+	else:
+		velocity.y += gravity * delta
+	handle_jump()
+	handle_double_jump()
+	handle_wall_jump()
 	move_and_slide()
