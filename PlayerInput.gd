e22a731dc26498e65a5558d7cc3802cae777f041
coy <coyoteandbird@gmail.com>
2024-01-18 23:54:26 -0500

    fix jump issue

--- PlayerInput.gd
+++ PlayerInput.gd
@@ -2,6 +2,9 @@ extends Node
 class_name PlayerInput
 
 ## Base class for Input nodes used with rollback.
+
+var _is_prev_frame_jump = false
+
 var is_jump_just_pressed = false
 var is_jump_just_released = false
 var horizontal_direction = 1
@@ -17,7 +20,8 @@ func _gather():
 	if not is_multiplayer_authority():
 		print("%s Tried to control %s" % [owner.name, multiplayer.get_unique_id()])
 		return
-	is_jump_just_pressed = Input.is_action_just_pressed("jump")
-	is_jump_just_released = Input.is_action_just_released("jump")
+	is_jump_just_pressed = Input.is_action_pressed("jump") and not _is_prev_frame_jump
+	is_jump_just_released = _is_prev_frame_jump and not Input.is_action_pressed("jump") 
 	is_holding_down = Input.is_action_pressed("down")
 	horizontal_direction = get_horizontal_axis()
+	_is_prev_frame_jump = Input.is_action_pressed("jump")
