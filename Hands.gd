fec090596fe451ef1b1bb8442d053701696f9221
Travis Compton <travisacompton06@gmail.com>
2024-01-27 09:41:36 -0500

    flip weapon

--- Hands.gd
+++ Hands.gd
@@ -3,7 +3,13 @@ extends Node2D
 @export var input: PlayerInput;
 
 func _process(delta):
+	var mousepos = get_global_mouse_position()
 	if !input.is_multiplayer_authority():
 		look_at(input.mouse_coordinates)
 	else:
-		look_at(get_global_mouse_position())
+		look_at(mousepos)
+	
+	if mousepos.x < global_position.x:
+		scale.y = -1
+	else:
+		scale.y = 1
